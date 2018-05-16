b1b1b611cb8a3e4db16a03f6cc246903eca0534b
Niklas Wilming <niklaswilming@fastmail.fm>
2018-05-16 10:06:43 +0200

    Move group2 to proper dir

--- group2/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='groupsp',
-      version='1',
-      description='The Base Players',
-      author='Sasa and Phil',
-      license='MIT',
-      packages=['groupsp'],
-      zip_safe=False)
